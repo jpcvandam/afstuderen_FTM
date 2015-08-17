9e7adfdee6c8ff6b627f5684859f9cf659b60649
jpcvandam <jpcvandam@gmail.com>
2015-08-17 21:24:37 +0200

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,16 @@
+#Het hoofdprogramma wat alle modules van het FTM moet aansturen
+#Auteur: John van Dam
+#Datum: 17 augustus 2015
+#imports van modules komen hier
+import os
+
+#haal de coördinaten van het gewenste punt op (uit een andere module of rechtstreeks bij de gebruiker van de webapp) en bewaar deze in een variabele
+
+#haal de benodigde bodemdata en weerdata die bij dat punt hoort op en stuur het door naar het 'rekenhart' FTM.py
+
+#haal de berekende grondwaterstand op bij het rekenhart en presenteer deze aan de gebruiker
+
+#[toekomstmuziek] stuur de grondwaterstand door naar een andere module die bodemvocht kan uitrekenen
+
+#[toekomstmuziek] haal het bodemvochtgehalte bij de module op en presenteer dit aan de gebruiker
+
