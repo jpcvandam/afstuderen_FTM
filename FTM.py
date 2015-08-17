d3f7f41a018df5151d697de5b500c3ca0f0fb2ac
jpcvandam <jpcvandam@gmail.com>
2015-08-17 21:06:14 +0200

    Create FTM.py

---
+++ FTM.py
@@ -0,0 +1,56 @@
+#dit script is de Python variant van het FTM door Jaco van der Gaast in Pascal
+#Auteur: John van Dam
+#Datum: 17 augustus 2015
+
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pylab
+import datetime
+import matplotlib.dates as mdates
+
+#functiedefinitie meteogegevens inladen
+def leesmeteo(meteobestand):
+    dfMeteo=pd.read_csv(meteobestand, names=['datum', 'neerslag', 'verdamping'], skipinitialspace=True, delim_whitespace=True, parse_dates=True, index_col=[0,1,2])
+    return dfMeteo
+
+
+#functiedefinitie rekenen
+#def waterbalans(dfMeteo.neerslag, dfMeteo.verdamping):
+#    dfMeteo.neerslagoverschot=dfMeteo.neerslag-dfMeteo.verdamping
+#    return dfMeteo.neerslagoverschot
+
+def gws_op_t(delta0, ht_dt, c, omega0, Pt) :
+    ht = c + delta0 * (ht_dt - c) + omega0 * Pt
+    return ht
+
+
+#haal eerst de coordinaten op van de locatie waar de grondwaterstand berekend moet worden, hierbij gelijk de b1 en b2 waarden uit de Staringreeks, tevens c, dt [kan misschien door gebruiker ingesteld worden, anders default 1], ht_dt [moet dus de dag ervoor berekend worden], Pt uit de waterbalans database
+
+def delta(dt, b2) :
+    delta1 = np.exp(-3.0 * dt/b2)
+    return(delta1)
+
+
+def omega(dt, b1, b2) :
+    omega1 = b1 * (1.0 - np.exp(-3.0 * dt/b2))
+    return(omega1)
+
+
+print omega(1.0, 0.1, 0.2)
+print delta(1.0, 0.2)
+print gws_op_t(delta(1.0, 0.2), 125.0, 130.0, omega(1.0, 0.1, 0.2), 2.0)
+
+#functiedefinitie gegevens wegschrijven
+#def schrijfmeteo(meteobestand_uit):
+#    dfMeteo.to_csv(meteobestand_uit, names=['datum', 'neerslag', 'verdamping'])
+    #return dfMeteo_uit
+
+#hier begint dan het programma
+print leesmeteo('METEO669.TXT') #bestandsextensies zijn case-senSiTive
+
+#dfMeteo.neerslagoverschot=dfMeteo.neerslag-dfMeteo.verdamping
+#print dfMeteo.neerslagoverschot
+
+#schrijfmeteo('meteobestand_uit.txt')
