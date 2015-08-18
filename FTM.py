b5eaa43b415e43a0529f482a24661b8ebc8a4400
jpcvandam <jpcvandam@gmail.com>
2015-08-18 21:16:40 +0200

    Update FTM.py

--- FTM.py
+++ FTM.py
@@ -13,7 +13,8 @@ import datetime as dt
 
 #functiedefinitie meteogegevens inladen
 def leesmeteo(meteobestand):
-    dfMeteo=pd.read_csv(meteobestand, names=['datum', 'neerslag', 'verdamping'], skipinitialspace=True, delim_whitespace=True, parse_dates=True, index_col=[0,1,2])
+    dfMeteo=pd.read_fwf(meteobestand, widths=[14,14,14,14,14], header = None, names = ['dag', 'maand' , 'jaar' ,'neerslag' , 'verdamping'], parse_dates={"Datetime" : [0,1,2]})
+    dfMeteo = dfMeteo.set_index('Datetime')
     return dfMeteo
 
 
@@ -22,26 +23,33 @@ def leesmeteo(meteobestand):
 #    dfMeteo.neerslagoverschot=dfMeteo.neerslag-dfMeteo.verdamping
 #    return dfMeteo.neerslagoverschot
 
-def gws_op_t(delta0, ht_dt, c, omega0, Pt) :
-    ht = c + delta0 * (ht_dt - c) + omega0 * Pt
-    return ht
+
 
 
 #haal eerst de coordinaten op van de locatie waar de grondwaterstand berekend moet worden, hierbij gelijk de b1 en b2 waarden uit de Staringreeks, tevens c, dt [kan misschien door gebruiker ingesteld worden, anders default 1], ht_dt [moet dus de dag ervoor berekend worden], Pt uit de waterbalans database
 
-def delta(dt, b2) :
-    delta1 = np.exp(-3.0 * dt/b2)
-    return(delta1)
+def delta(bergingscoefficient, drainageweerstand) :
+    delta = np.exp(-1.0 /( bergingscoefficient * drainageweerstand))
+    return(delta)
 
 
-def omega(dt, b1, b2) :
-    omega1 = b1 * (1.0 - np.exp(-3.0 * dt/b2))
-    return(omega1)
+def omega(drainageweerstand, bergingscoefficient): 
+    omega = drainageweerstand * (1.0 - delta(bergingscoefficient, drainageweerstand))
+    return(omega)
 
 
-print omega(1.0, 0.1, 0.2)
-print delta(1.0, 0.2)
-print gws_op_t(delta(1.0, 0.2), 125.0, 130.0, omega(1.0, 0.1, 0.2), 2.0)
+def cw(drainageweerstand, qbot, hgem):
+    cw = ((drainageweerstand * (qbot/10))*1) + hgem
+    return(cw)
+
+
+def gws_op_t(bergingscoefficient, drainageweerstand, ht_dt,  qbot, hgem, Pt) :
+    ht = cw(drainageweerstand, qbot, hgem) + delta(bergingscoefficient, drainageweerstand) * (ht_dt - cw(drainageweerstand, qbot, hgem)) + omega(drainageweerstand, bergingscoefficient) * Pt
+    return(ht)
+
+#print omega(1.0, 0.1, 0.2)
+#print delta(1.0, 0.2)
+#print gws_op_t(delta(1.0, 0.2), 125.0, 130.0, omega(1.0, 0.1, 0.2), 2.0)
 
 #functiedefinitie gegevens wegschrijven
 #het moet de oorspronkelijke datumlijst + het verschil tussen de kolommen 'neerslag' en 'verdamping' wegschrijven onder de naam neerslagoverschot
@@ -66,12 +74,57 @@ dfMeteo = dfMeteo.set_index('Datetime')
 #print leesmeteo('METEO669.TXT') #bestandsextensies zijn case-senSiTive
 print dfMeteo
 print dfMeteo.info()
+#arraypiet = dfMeteo['neerslag'].values
+#print arraypiet
 
 pd.DataFrame.plot(dfMeteo, kind='line')
+ax = pylab.gca()
+ax.set_ylabel('$mm/dag$')
+
 # without the line below, the figure won't show
 pylab.show()
 
-#dfMeteo.neerslagoverschot=dfMeteo.neerslag-dfMeteo.verdamping
+dfMeteo_out = pd.Series(dfMeteo.neerslag-dfMeteo.verdamping)
+#dfMeteo_out['neerslagoverschot']=dfMeteo.neerslag-dfMeteo.verdamping
+print dfMeteo_out
+meteobestand_uit = 'Meteo_out.csv'
+dfMeteo_out.to_csv(meteobestand_uit,  index=True, sep=',', header=True)
+
+array_neerslagoverschot = dfMeteo_out.values
+print array_neerslagoverschot
 #print dfMeteo.neerslagoverschot
 
-#schrijfmeteo('meteobestand_uit.txt')
+bodembestand = 'Bodemdata.txt'
+dfBodem=pd.read_csv(bodembestand, header=None, delimiter =',', names = ['#runn', 'al', 'hgem', 'drainw', 'berg', 'qbot' ]) #names = ['#runn', 'al', 'hgem', 'drainw', 'berg', 'qbot' ]
+print dfBodem
+
+array_drainweerstand = dfBodem['drainw'].values
+array_hgem = dfBodem['hgem'].values
+array_bergingscoefficient = dfBodem['berg'].values
+array_qbot = dfBodem['qbot'].values
+#hgem', 'drainw', 'berg', 'qbot
+
+#b = np.ndenumerate(array_grondwaterstand)
+#for position,value in b: print position-1,value # position is the N-dimensional index
+ht=0
+
+array_grondwaterstand = gws_op_t(array_bergingscoefficient, array_drainweerstand, ht, array_qbot, array_hgem, array_neerslagoverschot)
+
+
+
+print array_grondwaterstand
+
+dates = pd.date_range('19700101', periods=11323)
+
+
+dfGWS = pd.Series(array_grondwaterstand, index=dates)
+print dfGWS
+GWSbestand_uit = 'GWS_out.csv'
+#GWSdates=dfMeteo['Dag'].combineAdd(dfGWS)
+dfGWS.to_csv(GWSbestand_uit,  index=True, sep=',', header=None)
+pd.Series.plot(dfGWS, kind='line')
+ax = pylab.gca()
+ax.set_ylabel('$cm-mv$')
+
+# without the line below, the figure won't show
+pylab.show()
