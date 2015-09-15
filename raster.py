360a5193fa9ebb073a8cb4bf552797fa14a96f15
jpcvandam <jpcvandam@gmail.com>
2015-09-15 10:31:13 +0200

    Update raster.py

--- raster.py
+++ raster.py
@@ -3,15 +3,23 @@
 #Datum: 15 september 2015
 
 import subprocess
-x= "6.510"
-y= "53.156"
 
-berg = subprocess.check_output(["gdallocationinfo","-valonly", "/home/john/ftm/ftm/ftm/data/Bergingscoefficient.tif", x, y])
+def raster_q(bestand, x1, y1):
+    waarde = subprocess.check_output(["gdallocationinfo","-valonly","-geoloc", bestand, x1, y1])
+    return waarde
+
+
+x= "6.4569"
+y= "53.2149"
+
+berg = subprocess.check_output(["gdallocationinfo","-valonly","-geoloc", "/home/john/ftm/ftm/ftm/data/Bergingscoefficient1.tif", x, y])
 print berg
 
-drainw = subprocess.check_output(["gdallocationinfo","-valonly", "/home/john/ftm/ftm/ftm/data/Drainageweerstand.tif", x, y])
+drainw = subprocess.check_output(["gdallocationinfo","-valonly", "-geoloc","/home/john/ftm/ftm/ftm/data/Drainageweerstand1.tif", x, y])
 print drainw
-q_bot = subprocess.check_output(["gdallocationinfo","-valonly", "/home/john/ftm/ftm/ftm/data/Kwel-kk-nz.tif", x, y])
+q_bot = subprocess.check_output(["gdallocationinfo","-valonly","-geoloc", "/home/john/ftm/ftm/ftm/data/Kwel-kk-nz1.tif", x, y])
 print q_bot
-ontw = subprocess.check_output(["gdallocationinfo","-valonly", "/home/john/ftm/ftm/ftm/data/Ontwateringsbasis.tif", x, y])
+ontw = subprocess.check_output(["gdallocationinfo","-valonly","-geoloc", "/home/john/ftm/ftm/ftm/data/Ontwateringsbasis1.tif", x, y])
 print ontw
+
+print raster_q("/home/john/ftm/ftm/ftm/data/Drainageweerstand1.tif", x, y)
